04bf9e0543be788550c12a654e786e3734dd5d32
raza <raza.d3v@gmail.com>
2025-08-18 01:41:07 +0500

    cleanup unused components - v-1

--- babel.config.js
+++ babel.config.js
@@ -24,7 +24,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     ['@babel/plugin-transform-export-namespace-from'],
-    ['react-native-reanimated/plugin', {}, 'reanimated-plugin'],
     ['react-native-worklets/plugin', {}, 'worklets-plugin'],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
   ],
--- src/screens/main/Profile.tsx
+++ src/screens/main/Profile.tsx
@@ -1,5 +1,5 @@
 // src/screens/customer/Profile.tsx
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   View,
   Text,
